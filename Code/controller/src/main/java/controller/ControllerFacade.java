bb769cdc1e183311377c50decbb382838af2893b
PaulGouze <PaulGouze@github.com>
2018-06-06 08:52:25 +0200

    LORANN Final beta 1
    Code working, most modules ok...

--- Code/controller/src/main/java/controller/ControllerFacade.java
+++ Code/controller/src/main/java/controller/ControllerFacade.java
@@ -2,8 +2,10 @@ package controller;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import javax.swing.JOptionPane;
+
 import model.Level;
-import model.Example;
 import model.IModel;
 import view.IView;
 
@@ -15,11 +17,14 @@ import view.IView;
  */
 public class ControllerFacade implements IController {
 
+
+    int[] choices = null;
     /** The view. */
     private final IView  view;
 
     /** The model. */
     private final IModel model;
+
 
     /**
      * Instantiates a new controller facade.
@@ -31,9 +36,11 @@ public class ControllerFacade implements IController {
      */
     public ControllerFacade(final IView view, final IModel model) {
         super();
+        
         this.view = view;
         this.model = model;
     }
+    
 
     /**
      * Start.
@@ -42,7 +49,7 @@ public class ControllerFacade implements IController {
      *             the SQL exception
      */
     public void start() throws SQLException {
-        this.getView().displayMessage(this.getModel().getExampleById(1).toString());
+        /*this.getView().displayMessage(this.getModel().getExampleById(1).toString());
 
         this.getView().displayMessage(this.getModel().getExampleByName("Example 2").toString());
 
@@ -52,16 +59,86 @@ public class ControllerFacade implements IController {
             message.append(example);
             message.append('\n');
         }
-        this.getView().displayMessage(message.toString());
+*/
+        final List<Level> level1 = this.getModel().getLevel1();
+        final List<Level> level2 = this.getModel().getLevel2();
+        final List<Level> level3 = this.getModel().getLevel3();
+        final List<Level> level4 = this.getModel().getLevel4();
+        final List<Level> level5 = this.getModel().getLevel5();
+        
+        
+        String[] choices = { "Quitter","1", "2", "3", "4", "5" };
+        int choice = JOptionPane.showOptionDialog(null, "Choisissez votre niveau",
+            "Choose Level", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, choices, choices[0]);
+     
+        System.out.println(choice);
+        //choice = this.getView().ScanMap();
+        if (choice ==1) {
+        for(int i = 0;i<level1.size();i++) {
+	        System.out.print("I = "+i+" ");
+	        System.out.println(level1.get(i).getList());
+	        this.view.Map(level1.get(i).getList());
+	       
+	               }
+        
+        }
+        
+        if (choice ==2) {
+            for(int i = 0;i<level2.size();i++) {
+    	        System.out.print("I = "+i+" ");
+    	        System.out.println(level2.get(i).getList());
+    	        this.view.Map(level2.get(i).getList());
+            }
+            }
+        
+        if (choice ==3) {
+            for(int i = 0;i<level3.size();i++) {
+    	        System.out.print("I = "+i+" ");
+    	        System.out.println(level3.get(i).getList());
+    	        this.view.Map(level3.get(i).getList());
+            }
+            }
+     
+        
+        if (choice ==4) {
+            for(int i = 0;i<level4.size();i++) {
+    	        System.out.print("I = "+i+" ");
+    	        System.out.println(level4.get(i).getList());
+    	        this.view.Map(level4.get(i).getList());
+            }
+            }
+        if (choice ==5) {
+            for(int i = 0;i<level5.size();i++) {
+    	        System.out.print("I = "+i+" ");
+    	        System.out.println(level5.get(i).getList());
+    	        this.view.Map(level5.get(i).getList());
+            }
+             
+ }
+        this.getView().Window();     
+        /*this.getView().displayMessage(message.toString());
         
         final List<Level> level1 = this.getModel().getLevel1();
         for(int i = 0;i<level1.size();i++) {
 	        System.out.print("I = "+i+" ");
 	        System.out.println(level1.get(i).getList());
 	        this.view.Map(level1.get(i).getList());
-	        }
-	      
-        this.getView().Window();
+        
+	        }*/
+        
+            /*this.getView().displayMessage(this.getModel().getExampleById(1).toString());
+            this.getView().displayMessage(this.getModel().getExampleByName("Example 2").toString());
+            final List<Example> examples = this.getModel().getAllExamples();
+            final StringBuilder message = new StringBuilder();
+            for (final Example example : examples) {message.append(example);message.append('\n');}
+            this.getView().displayMessage(message.toString());*/
+        	
+        	/**
+        	 * Load all map in a list*/
+                    
+            
+            
+     
         }
         
     
--- Code/model/src/main/java/model/ModelFacade.java
+++ Code/model/src/main/java/model/ModelFacade.java
@@ -2,7 +2,6 @@ package model;
 
 import java.sql.SQLException;
 import java.util.List;
-
 import model.dao.ExampleDAO;
 import model.dao.LevelDAO;
 
@@ -25,7 +24,6 @@ public final class ModelFacade implements IModel {
      * (non-Javadoc)
      * @see model.IModel#getExampleById(int)
      */
-    @Override
     public Example getExampleById(final int id) throws SQLException {
         return ExampleDAO.getExampleById(id);
     }
@@ -34,7 +32,7 @@ public final class ModelFacade implements IModel {
      * (non-Javadoc)
      * @see model.IModel#getExampleByName(java.lang.String)
      */
-    @Override
+   
     public Example getExampleByName(final String name) throws SQLException {
         return ExampleDAO.getExampleByName(name);
     }
@@ -43,27 +41,29 @@ public final class ModelFacade implements IModel {
      * (non-Javadoc)
      * @see model.IModel#getAllExamples()
      */
-    @Override
+ 
     public List<Example> getAllExamples() throws SQLException {
         return ExampleDAO.getAllExamples();
     }
-    @Override
+
     public List<Level> getLevel1() throws SQLException {
         return LevelDAO.getLevel1();
     }
-    @Override
+    
     public List<Level> getLevel2() throws SQLException {
         return LevelDAO.getLevel2();
-    }@Override
+    }
     public List<Level> getLevel3() throws SQLException {
         return LevelDAO.getLevel3();
     }
-    @Override
     public List<Level> getLevel4() throws SQLException {
         return LevelDAO.getLevel4();
     }
-    @Override
     public List<Level> getLevel5() throws SQLException {
         return LevelDAO.getLevel5();
     }
+    
+    
+    
+   
 }
